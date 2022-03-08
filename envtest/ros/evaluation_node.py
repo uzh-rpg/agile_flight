0c66b2dd23ec46d58685b5e7e9edfb6f545afc45
Leonard Bauersfeld <leonard.bauersfeld@gmail.com>
2022-03-08 19:59:11 +0100

    Added evaluation

---
+++ envtest/ros/evaluation_node.py
@@ -0,0 +1,146 @@
+import os
+import re
+import sys
+import yaml
+import rospy
+import numpy as np
+
+from agiros_msgs.msg import Command, QuadState
+from envsim_msgs.msg import ObstacleArray
+from std_msgs.msg import Empty
+
+from uniplot import plot
+
+class Evaluator:
+    def __init__(self, config):
+        rospy.init_node("evaluator", anonymous=False)
+        self.config = config
+
+        self.xmax = int(self.config['target'])
+
+        self.is_active = False
+        self.pos = []
+        self.dist = []
+        self.time_array = (self.xmax+1)*[np.nan]
+
+        self.hit_obstacle = False
+        self.crash = 0
+        
+        self._initSubscribers(config['topics'])
+        self._initPublishers(config['topics'])
+
+
+    def _initSubscribers(self, config):
+        self.state_sub = rospy.Subscriber(
+                "/%s/%s" % (config['quad_name'], config['state']),
+                QuadState, 
+                self.callbackState,
+                queue_size=1,
+                tcp_nodelay=True)
+        
+        self.obstacle_sub = rospy.Subscriber(
+                "/%s/%s" % (config['quad_name'], config['obstacles']),
+                ObstacleArray, 
+                self.callbackObstacles,
+                queue_size=1,
+                tcp_nodelay=True)
+
+        self.start_sub = rospy.Subscriber(
+                "/%s/%s" % (config['quad_name'], config['start']),
+                Empty, 
+                self.callbackStart,
+                queue_size=1,
+                tcp_nodelay=True)
+
+
+    def _initPublishers(self, config):
+        self.finish_pub = rospy.Publisher(
+                "/%s/%s" % (config['quad_name'], config['finish']),
+                Empty,
+                queue_size=1,
+                tcp_nodelay=True)
+
+
+    def publishFinish(self):
+        self.finish_pub.publish()
+        self.printSummary()
+
+
+    def callbackState(self, msg):
+        if not self.is_active:
+            return
+
+        self.pos.append(np.array(
+                        [msg.header.stamp.to_sec(),
+                         msg.pose.position.x,
+                         msg.pose.position.y,
+                         msg.pose.position.z]
+                        ))
+
+        pos_x = msg.pose.position.x
+        bin_x = int(max(min(np.floor(pos_x),self.xmax),0))
+        if np.isnan(self.time_array[bin_x]):
+            self.time_array[bin_x] = rospy.get_rostime().to_sec()
+
+        if pos_x > self.xmax:
+            self.is_active = False
+            self.publishFinish()
+
+
+    def callbackStart(self, msg):
+        self.is_active = True
+        self.time_array[0] = rospy.get_rostime().to_sec()
+
+
+    def callbackObstacles(self, msg):
+        if not self.is_active:
+            return
+        
+        obs = msg.obstacles[0]
+        dist = np.linalg.norm(np.array([obs.position.x, 
+                                        obs.position.y, 
+                                        obs.position.z]))
+        margin = dist - obs.scale/2
+        self.dist.append([msg.header.stamp.to_sec(), margin])
+        if margin < 0:
+            if not self.hit_obstacle:
+                self.crash += 1
+            self.hit_obstacle = True
+        else:
+            self.hit_obstacle = False
+
+
+    def printSummary(self):
+        ttf = self.time_array[-1] - self.time_array[0]
+        print("You reached the goal in %5.3f seconds" % ttf)
+        print("Your intermediate times are:")
+        print_distance = 10
+        for i in range(print_distance, self.xmax-print_distance+1, print_distance):
+            print("    %2i: %5.3fs " % (i,self.time_array[i] - self.time_array[0]))
+        print("You hit %i obstacles" % self.crash)
+
+        if not self.config['plots']:
+            return
+
+        print("Here is a plot of your trajectory in the xy plane")
+        pos = np.array(self.pos)
+        plot(xs=pos[:,1], ys=pos[:,2], color=True)
+
+        print("Here is a plot of your average velocity per 1m x-segment")
+        x = np.arange(1,self.xmax+1)
+        dt = np.array(self.time_array)
+        y = 1/(dt[1:]-dt[0:-1])
+        plot(xs=x, ys=y, color=True)
+        
+        print("Here is a plot of the distance to the closest obstacles")
+        dist = np.array(self.dist)
+        plot(xs=dist[:,0]-self.time_array[0], ys=dist[:,1], color=True)
+
+
+
+if __name__=="__main__":
+    with open("./evaluation_config.yaml") as f:
+        config = yaml.safe_load(f)
+
+    Evaluator(config)
+    rospy.spin()
