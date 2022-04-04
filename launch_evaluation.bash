fbc391d897c1f62d15ebec5f8b7794f08d1fff0f
Michael Bosello <michael.bosello@unibo.it>
2022-04-04 18:08:53 +0200

    Fix launch by adding publish to start topic

--- launch_evaluation.bash
+++ launch_evaluation.bash
@@ -35,6 +35,7 @@ do
   rostopic pub /kingfisher/dodgeros_pilot/off std_msgs/Empty "{}" --once
   rostopic pub /kingfisher/dodgeros_pilot/reset_sim std_msgs/Empty "{}" --once
   rostopic pub /kingfisher/dodgeros_pilot/enable std_msgs/Bool "data: true" --once
+  rostopic pub /kingfisher/dodgeros_pilot/start std_msgs/Empty "{}" --once
 
   export ROLLOUT_NAME="rollout_""$i"
   echo "$ROLLOUT_NAME"
